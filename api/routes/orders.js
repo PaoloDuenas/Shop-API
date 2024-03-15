804f8e607a32bb2215ee509c1994cb1fd37ed620
PaoloDuenas <paolo.duenas@gmail.com>
2024-03-14 23:43:09 -0500

    order functions

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,129 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-//handle incoming GET requests to /orders
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
+const Order = require("../models/order");
+const Product = require("../models/product");
+
+//Obetener la lista de ordenes
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select("product quantity _id")
+    .populate('product')
+    .exec()
+    .then((docs) => {
+      res.status(200).json({
+        count: docs.length,
+        order: docs.map((doc) => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: "https://localhost:3000/orders/" + doc._id,
+            },
+          };
+        }),
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
-router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order was created',
-        order: order
+//Postear ordenes, (funcionando)
+router.post("/", (req, res, next) => {
+  const productId = req.body.productId;
+  const quantity = req.body.quantity;
+
+  Product.findById(productId)
+    .then((product) => {
+      if (!product) {
+        return res.status(404).json({
+          message: "Product not found",
+        });
+      }
+
+      const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        quantity: quantity,
+        product: productId,
+      });
+
+      return order.save();
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Order stored",
+        createdOrder: {
+          _id: result._id,
+          product: result.product,
+          quantity: result.quantity,
+        },
+        request: {
+          type: "GET",
+          url: "https://localhost:3000/orders/" + result._id,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
-router.get('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderId: req.params.orderId
+// Obetener una orden especifica por su ID
+router.get("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
+  Order.findById(id)
+    .select("product quantity _id")
+    .populate("product")
+    .exec()
+    .then((doc) => {
+      if (doc) {
+        res.status(200).json({
+          order: doc,
+          request: {
+            type: "GET",
+            url: "https://localhost:3000/orders/" + doc._id,
+          },
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for provided ID" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
 });
 
-router.delete('/:orderId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Order was deleted',
-        orderId: req.params.orderId
+//Borrar orden por su ID
+router.delete("/:orderId", (req, res, next) => {
+  Order.deleteOne({ _id: req.params.orderId })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Order deleted",
+        request: {
+          type: "POST",
+          url: "https://localhost:3000/orders/",
+          body: { productId: "ID", quantity: "Number" },
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 
-
 //Atlas username: paoloduenas
 //Atlas password: 9CpOksPZQyee9Jz6
 
-//Atlas Admin username: Admin   
+//Atlas Admin username: Admin
 //Atlas Admin password: lQUJvW8YeRevd5UE
